67b992892b37bfebf1d1ca297975254d3c4c19b2
Julian.Qian <Julian.Qian@0bb4c7ca-9331-11dd-8a4d-25b79fea6e5d>
2008-10-11 03:50:32 +0000

    new

--- trunk/sim.java
+++ trunk/sim.java
@@ -20,39 +20,52 @@ public class sim {
     }
 
 	@SuppressWarnings("unchecked")
-
+// -c cache size -a access file -u user config file
 	public static void main(String[] args) {
     	String asPath = "/Volumes/src/jxu/sim/src/as";
-        //for
+    	int cacheSize = 50;
+    	String asFile = "/Volumes/src/jxu/sim/src/as/query100k-data688-10days-uniform.txt.as";
+    	String uFile;
+    	String oFile;
+        for (int i = 0; i<args.length; ++i) {
+        	String s = args[i];
+        	if (s == "-c") {
+        		++i;
+        		cacheSize = Integer.parseInt(args[i]);
+        	} else if (s == "-u") {
+        		++i;
+        		uFile = args[i];
+        	} else if (s == "-a") {
+        		++i;
+        		asFile = args[i];
+        	} else if (s == "-o") {
+        		++i;
+        		oFile = args[i];
+        	}
+        }
 		// Server
 		int serverSize = 10;
 		Server[] s = Server.getServers(serverSize);
 		// User
 
 		// Access
-        String[] asFiles = getListFiles(asPath);
-        for (int i = 0; i < asFiles.length; ++i) {
-        	java.io.File one = new java.io.File(asFiles[i]);
-        	if (one.isFile()) {
-        		// Data can't be cloned, since data's location changed during running
-        		Data[] d = Data.getDatas(s);
-        		// update
-            	ArrayList<Event> e = new ArrayList<Event>(40000);
-            	Update.getUpdate(d, e);
-        		// avoid the .svn folder
-        		// read access from disk
-            	ArrayList<Access> a = new ArrayList<Access>(20000);
-        		Access.getAccess(d, a, asFiles[i]);
+		// Data can't be cloned, since data's location changed during running
+		Data[] d = Data.getDatas(s);
+		// update
+    	ArrayList<Event> e = new ArrayList<Event>(40000);
+    	Update.getUpdate(d, e);
+		// avoid the .svn folder
+		// read access from disk
+    	ArrayList<Access> a = new ArrayList<Access>(20000);
+		Access.getAccess(d, a, asFile);
 
 
-        		e.addAll(a);
-        		Collections.sort(e);
+		e.addAll(a);
+		Collections.sort(e);
 
-        		//User[] u = User.getUsers();
-                Cache c = new Cache();
-                c.init(50, e, d, s);
-        	}
-        }
+		//User[] u = User.getUsers();
+        Cache c = new Cache();
+        c.init(cacheSize, e, d, s);
 	}
 
 }
